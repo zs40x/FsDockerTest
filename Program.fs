e962db936c1180ead83adfc9671111d2e65b54ab
Stefan Mehnert <s.mehnert@polipol.de>
2022-09-30 12:53:20 +0200

    Added a Function

--- Program.fs
+++ Program.fs
@@ -1,2 +1,10 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let generateRandomNumber() =
+    let r = System.Random()
+    r.Next()
+
 printfn "Hello from F#"
+
+let number = generateRandomNumber()
+printfn "Another Number: %i" number
