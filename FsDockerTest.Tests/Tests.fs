14f6299a5238dfd66c10d841928f11ffad0d3db4
Stefan Mehnert <s.mehnert@polipol.de>
2022-09-30 13:47:09 +0200

    Added a Unit-Test Project

---
+++ FsDockerTest.Tests/Tests.fs
@@ -0,0 +1,11 @@
+namespace FsDockerTest.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
