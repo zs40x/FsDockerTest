060af13c0bb920de171517cb55343e8d95ac8f4d
scx <stefanmehnert@gmail.com>
2022-10-02 07:40:56 +0200

    Added additional Tests.

--- FsDockerTest.Tests/FizzBuzzTests.fs
+++ FsDockerTest.Tests/FizzBuzzTests.fs
@@ -1,4 +1,4 @@
-namespace MathService.Tests
+namespace testApp.FizzBuzzTests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
@@ -10,3 +10,15 @@ type FizzBuzzTests () =
     [<TestMethod>]
     member this.OneIsOne() =
         Assert.AreEqual("1", fizzBuzz 1)
+
+    [<TestMethod>]
+    member this.ThreeIsFizz() =
+        Assert.AreEqual("Fizz", fizzBuzz 3)
+
+    [<TestMethod>]
+    member this.FiveIsBuzz() =
+        Assert.AreEqual("Buzz", fizzBuzz 5)
+
+    [<TestMethod>]
+    member this.FivteenIsFizzBuzz() =
+        Assert.AreEqual("FizzBuzz", fizzBuzz 15)
