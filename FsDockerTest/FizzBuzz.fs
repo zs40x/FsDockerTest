e191b758c8bdee673983d7a1bd1b9444758d6eee
scx <stefanmehnert@gmail.com>
2022-10-02 08:01:30 +0200

    Replaced if-elif-else with Pattern-Matching.

--- FsDockerTest/FizzBuzz.fs
+++ FsDockerTest/FizzBuzz.fs
@@ -2,11 +2,8 @@ namespace testApp
 
 module FizzBuzz = 
     let fizzBuzz (number: int) : string =
-        if number % 3 = 0 && number % 5 = 0 then
-            "FizzBuzz"
-        elif number % 3 = 0 then
-            "Fizz"
-        elif number % 5 = 0 then
-            "Buzz"
-        else
-            sprintf "%i" number
+        match number with
+        | (n) when n % 3 = 0 && n % 5 = 0 -> "FizzBuzz"
+        | (n) when n % 3 = 0 -> "Fizz"
+        | (n) when n % 5 = 0 -> "Buzz"
+        | _ ->sprintf "%i" number
